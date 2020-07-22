5e3740a9ac8bb6d4ceae970e3ebe424d98e1c940
Anthony Tarbinian <42757207+atar13@users.noreply.github.com>
2020-07-22 15:15:40 -0700

    Posts a christmas image on christmas day!

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@
 const Twit = require('twit');
 const express = require('express');
 const app = express();
+const fs = require('fs-extra');
 
 const config = require('./config');
 
@@ -49,9 +50,10 @@ function newTweet(){
 
     daysLeft = remainingDays;
 
+    var daysUntilEndofYear = parseInt(daysLeft) +6;
 
 
-    var possibleMessages = [`${daysLeft} days left until Christmas!`];
+    var possibleMessages = [`${daysLeft} days left until Christmas!`,`${daysLeft} days until Christmas and ${daysUntilEndofYear} days until the end of ${currentChristmasYear()}! `];
 
 
 
@@ -69,26 +71,41 @@ function newTweet(){
             if(dayMonthYearEqual(lastTweetDate, currentDate)){
 
             }else{
-                //maybe add a 1/10 chance that the message also includes how many days until the end of 2020
-                var tweet = {
-                    status: possibleMessages[0]
-                }
+
+                var random = (Math.random()*10).toFixed(0);
+
 
                 var tweetMessage;
                 if(currentDate.getMonth()!=11||currentDate.getDate()<25||currentDate.getDate()>25){
-                    tweetMessage = tweet.status;
-                }
-                else{
+                    if(random == 5 ){
+                        tweetMessage = possibleMessages[1];
+                    }else{
+                        tweetMessage = possibleMessages[0];
+                    }        
+                    T.post('statuses/update', tweet, tweeted);
+        
+                }else{
                     tweetMessage = 'Today is Christmas! Merry Christmas!';
+
+                    var christmasImage = fs.readFileSync('./christmasImage.jpg', {encoding: 'base64'});
+                    T.post('media/upload', {media_data: christmasImage}, (err, data, response) => {
+                        var imageId = data.media_id_string;
+                        var altText = `Merry Christmas ${currentChristmasYear}!`;
+                        var meta_params = {media_id: imageId, alt_text: { text: altText}};
+                        
+                        T.post('media/metadata/create', meta_params, (err, data, response) => {
+                            if(!err) {
+                                var params = {status: tweetMessage, media_ids:[imageId]};
+
+                                T.post('statuses/update', params, (err, data, repsonse) =>{
+                                });
+                            }
+                        });
+                    });
+                    
                 }
 
-                // if(dayMonthYearEqual(currentDate, christmasDay)){
-                //     tweetMessage = 'Today is Christmas! Merry Christmas!';
-                // }else{
-                //     tweetMessage = tweet.status;
-                // }
 
-                // T.post('statuses/update', tweet, tweeted);
                 console.log('About to tweet: ' + tweetMessage);
 
 
@@ -120,7 +137,8 @@ function newTweet(){
 
 
 
-// newTweet();
+
+
 
 app.listen(PORT, () =>{
     console.log(`Twitter bot is running at http://localhost:${PORT}`);
